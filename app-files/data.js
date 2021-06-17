34bf4fad82744cef75c81a700a8bc56d4efce51e
Jerome Maurey-Delaunay <jerome.maureydelaunay@gmail.com>
2021-06-17 21:26:37 +0100

    Added initial tour created with https://www.marzipano.net/

---
+++ app-files/data.js
@@ -0,0 +1,300 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-formahouse_360s_web_0",
+      "name": "FormaHouse_360s_Web_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.607061843779718,
+        "pitch": 0.003282682911180501,
+        "fov": 1.6257365617269903
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1770183264870067,
+          "pitch": 0.03635102741488083,
+          "rotation": 0,
+          "target": "1-formahouse_360s_web_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-formahouse_360s_web_1",
+      "name": "FormaHouse_360s_Web_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.5889924008624448,
+        "pitch": -0.04996525671605312,
+        "fov": 1.6257365617269903
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19996732287320285,
+          "pitch": 0.07461693260095537,
+          "rotation": 0,
+          "target": "2-formahouse_360s_web_2"
+        },
+        {
+          "yaw": 1.819259709900197,
+          "pitch": 0.0899507142416276,
+          "rotation": 0,
+          "target": "0-formahouse_360s_web_0"
+        },
+        {
+          "yaw": 1.4394407737921213,
+          "pitch": 0.13275095790686464,
+          "rotation": 0,
+          "target": "3-formahouse_360s_web_3"
+        },
+        {
+          "yaw": 1.3077998816832448,
+          "pitch": -0.1401919023110878,
+          "rotation": 0,
+          "target": "4-formahouse_360s_web_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-formahouse_360s_web_2",
+      "name": "FormaHouse_360s_Web_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7149876590267112,
+          "pitch": 0.07441281571607306,
+          "rotation": 0,
+          "target": "1-formahouse_360s_web_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-formahouse_360s_web_3",
+      "name": "FormaHouse_360s_Web_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6805316277107654,
+          "pitch": 0.028472809799056975,
+          "rotation": 0,
+          "target": "1-formahouse_360s_web_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-formahouse_360s_web_4",
+      "name": "FormaHouse_360s_Web_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7523119936590064,
+          "pitch": 0.11470641936978865,
+          "rotation": 0,
+          "target": "5-formahouse_360s_web_5"
+        },
+        {
+          "yaw": -1.7840267325587185,
+          "pitch": 0.33409055589227243,
+          "rotation": 0,
+          "target": "1-formahouse_360s_web_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-formahouse_360s_web_5",
+      "name": "FormaHouse_360s_Web_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5725923216422562,
+          "pitch": 0.16040863093932423,
+          "rotation": 0,
+          "target": "4-formahouse_360s_web_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-formahouse_360s_web_6",
+      "name": "FormaHouse_360s_Web_6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007305939730320787,
+          "pitch": 0.22922860329543937,
+          "rotation": 0,
+          "target": "4-formahouse_360s_web_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-formahouse_360s_web_7",
+      "name": "FormaHouse_360s_Web_7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Unity Forma House",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
